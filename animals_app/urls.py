5dff56b5cd2895981e587b1aa318d233eb0c5897
Alex <Alekseytorshin@gmail.com>
2024-10-25 15:56:09 +0300

    Добавлена главная страница. Добавлен базовый шаблон с пунктом меню, поиском, а также выпадающим списком.

--- animals_app/urls.py
+++ animals_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import view_animals
+
 
 app_name = "animals"
 
 urlpatterns = [
-    
+    path('', view_animals, name='index')
 ]
--- animals_app/views.py
+++ animals_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def view_animals(request):
+    return render(request, "animals/index.html")
+
 # Create your views here.
