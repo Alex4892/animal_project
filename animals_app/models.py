bddde4daeef5b90fa2fed8c5f5aa8ce252cd6e0c
Alex <Alekseytorshin@gmail.com>
2024-10-29 00:15:16 +0300

    Добавлена модель Animal. Зарегистрирована в админ панели.

--- animals_app/models.py
+++ animals_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+class Animal(models.Model):
+    # kind = models.CharField(
+    #     max_length=100
+    # )
+    title = models.CharField(
+        max_length=200
+    )
+    description = models.TextField(
+        max_length=500
+    )
+    # publication = models.CharField(
+    #     max_length=100
+    # )
+    # publication_year = models.CharField(
+    #     max_length=4
+    # )
+    # price = models.DecimalField(
+    #     max_digits=10,
+    #     decimal_places=2
+    # )
+    # author = models.CharField(
+    #     max_length=100
+    # )
+    create_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Животное"
+        verbose_name_plural = "Животные"
+
 # Create your models here.
