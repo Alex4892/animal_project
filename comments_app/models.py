678d2ee6c3e35592da02925be80c67eab134f43a
Alex <Alekseytorshin@gmail.com>
2024-11-03 11:13:43 +0300

    Добавлена модель комментария. Создана миграция.

--- comments_app/models.py
+++ comments_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from animals_app.models import Animal
+
+class Comment(models.Model):
+    animal = models.ForeignKey(
+        Animal, on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    email = models.EmailField(
+        verbose_name='Email'
+    )
+    text = models.TextField(
+        verbose_name='Комментарий'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'Комментарий от {self.email} к объявлению о виде животном "{self.animal.name}"'
+
 
 # Create your models here.
