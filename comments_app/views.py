c3587a269d42bd623db0d330b879f4979c593150
Alex <Alekseytorshin@gmail.com>
2024-11-03 11:41:15 +0300

    Реализовано удаление и изменение комментария на странице объявления.

--- comments_app/views.py
+++ comments_app/views.py
@@ -16,4 +16,20 @@ def add_comment_view(request, animal_id):
         form = CommentForm()
     return redirect('animals:detail_animal', animal_id=animal.id)
 
+def edit_comment_view(request, comment_id, animal_id):
+    comment = get_object_or_404(Comment, id=comment_id, animal_id=animal_id)
+    animal = get_object_or_404(Animal, id=animal_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('animals:detail_animal', animal_id=animal_id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'comments/edit_comment.html', {'form': form, 'animal': animal, 'comment': comment})
+def delete_comment_view(request, comment_id, animal_id):
+    comment = get_object_or_404(Comment, id=comment_id, animal_id=animal_id)
+    comment.delete()
+    return redirect('animals:detail_animal', animal_id=animal_id)
+
 # Create your views here.
