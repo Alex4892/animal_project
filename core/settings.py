e68d66754e62e528a1857de0ea238fe87197fc21
Alex <Alekseytorshin@gmail.com>
2024-11-04 10:48:15 +0300

    Добавлена модель пользователя. Создана миграция.

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'animals_app',
     'comments_app',
     'users',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from phonenumber_field.modelfields import PhoneNumberField
+class User(AbstractUser):
+    phone_number = PhoneNumberField(
+        'Номер телефона',
+        unique=True,
+    )
+    father_name = models.CharField(
+        'Отчество',
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        'Фотография',
+        upload_to='users/',
+        blank=True
+    )
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 # Create your models here.
