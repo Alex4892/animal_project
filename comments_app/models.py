c3430539b4742eb52dba033b23739627cfcd2c3e
Alex <Alekseytorshin@gmail.com>
2024-11-04 13:34:26 +0300

    Изменена модель комментария. Создана миграция.

--- comments_app/models.py
+++ comments_app/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from animals_app.models import Animal
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Comment(models.Model):
     animal = models.ForeignKey(
         Animal, on_delete=models.CASCADE,
         related_name='comments'
+    )
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='comments',
+        verbose_name='Автор'
     )
     email = models.EmailField(
         verbose_name='Email'
@@ -15,6 +26,10 @@ class Comment(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата создания'
+    )
+    is_verified = models.BooleanField(
+        default=False,
+        verbose_name='Проверен администратором'
     )
     class Meta:
         verbose_name = 'Комментарий'
