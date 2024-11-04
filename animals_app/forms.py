9d78dd0e49d203d0d8964ebda59cd020ff4289af
Alex <Alekseytorshin@gmail.com>
2024-11-04 21:36:09 +0300

    Удалено поле с количеством, т.к., не влияет на логику сайта. Применена миграция.

--- animals_app/forms.py
+++ animals_app/forms.py
@@ -3,7 +3,7 @@ from .models import Animal
 class AnimalForm(forms.ModelForm):
     class Meta:
         model = Animal
-        fields = ['title', 'kind', 'nickname', 'description', 'phone_number', 'signs', 'image', 'quantity']
+        fields = ['title', 'kind', 'nickname', 'description', 'phone_number', 'signs', 'image']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'kind': forms.SelectMultiple(attrs={'class': 'form-control'}),
@@ -11,6 +11,6 @@ class AnimalForm(forms.ModelForm):
             'description': forms.Textarea(attrs={'class': 'form-control'}),
             'phone_number': forms.NumberInput(attrs={'class': 'form-control'}),
             'signs': forms.TextInput(attrs={'class': 'form-control'}),
-            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+            # 'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control', 'name':'image'}),
         }
