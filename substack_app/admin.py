8e521661bc75808ae896b88871b38047d5a9efa9
Alex <Alekseytorshin@gmail.com>
2024-11-09 11:25:28 +0300

    Добавлена модель закладки. Создана и применена миграция.

--- substack_app/admin.py
+++ substack_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Substack
+
+admin.site.register(Substack)
+
 # Register your models here.
--- substack_app/models.py
+++ substack_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+from animals_app.models import Animal
+
+
+User = get_user_model()
+class Substack(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='substack'
+    )
+    animal = models.ForeignKey(
+        Animal,
+        on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField(
+        default=1
+    )
+    class Meta:
+        unique_together = ('user', 'animal')
+        verbose_name = "Закладка"
+        verbose_name_plural = "Закладки"
+    def __str__(self):
+        return f"{self.user.username} - {self.animal.name} (x{self.quantity})"
 
 # Create your models here.
