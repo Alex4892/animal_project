1508e2a21c4324118433b513c5ea65a82fe0094c
Alex <Alekseytorshin@gmail.com>
2024-11-09 11:21:47 +0300

    Добавлен шаблон и функция представления для страницы с закладкой.

--- substack_app/urls.py
+++ substack_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from .views import view_substack
+
 app_name = "substack"
 
 urlpatterns = [
-
+    path('substack/', view_substack, name='substack'),
 ]
--- substack_app/views.py
+++ substack_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def view_substack(request):
+    return render(request, 'substack/substack.html')
+
 # Create your views here.
